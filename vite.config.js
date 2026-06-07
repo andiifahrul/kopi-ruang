ce2c8f63f1fe07a32c5a66cf7af67ce914a706fb
andiifahrul <andiahmadfahrul@gmail.com>
2026-06-07 19:43:16 +0800

    Konfigurasi untuk deploy ke Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kopi-ruang/',
+  // base: '/kopi-ruang/', // Jadikan komentar atau hapus baris ini untuk Vercel
 })
